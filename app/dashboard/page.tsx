12ce68e1df0c9bfab99a377989092866947e73d5
Nursan Omarov <Nursan.Omarov@audiit.com>
2026-05-01 20:49:34 +0500

    add

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,20 +5,20 @@ import { headers } from 'next/headers'
 import { Dashboard } from './components/Dashboard'
 import { getLatestAgentRun, getUnreadEmails, getUser, getUserIntegrations } from '../db/queries';
 
- const DashboardPage = async () => {
+const DashboardPage = async () => {
   const session = await auth.api.getSession({
-                headers: await headers(),
-              });
- 
+    headers: await headers(),
+  });
+
 
   const userId = session?.user.id;
   const latestRun = await getLatestAgentRun(userId!)
   const userIntegrations = await getUserIntegrations(userId!)
   const User = await getUser(userId!)
-  const {emailsProcessed, draftsCreated, tasksCreated} = await getUnreadEmails(userId!)
+  const { emailsProcessed, draftsCreated, tasksCreated } = await getUnreadEmails(userId!)
 
   return (
-   <Dashboard emailsProcessed={emailsProcessed} draftsCreated={draftsCreated} tasksCreated={tasksCreated} User={User} latestRun={latestRun} userIntegrations={userIntegrations}/>
+    <Dashboard emailsProcessed={emailsProcessed} draftsCreated={draftsCreated} tasksCreated={tasksCreated} User={User} latestRun={latestRun} userIntegrations={userIntegrations} />
   )
 }
 
